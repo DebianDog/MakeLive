9462b309842589027cd8b539eb1729c1dda7fe10
fredx181 <f.blom18@chello.nl>
2017-11-03 20:29:50 +0100

    Stretch support, changes

--- dog-boot-stretch/usr/local/bin/chrome-root.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/bin/google-chrome-stable --user-data-dir=/root/chrome/user --disk-cache-dir="/root/chrome/cache" --disk-cache-size=10000000 --media-cache-size=10000000 --no-sandbox --disable-infobars "$@"
